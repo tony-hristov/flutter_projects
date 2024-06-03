90998936da4140b66924bdab2754ac9e62f68eb0
Tony Hristov <tony.hristov@alkami.com>
2024-06-03 16:21:33 -0500

    working on score_ring

--- flutter_ui_components/flutter_ui_components/lib/src/components/score_ring/src/ring_painter.dart
+++ flutter_ui_components/flutter_ui_components/lib/src/components/score_ring/src/ring_painter.dart
@@ -2,52 +2,70 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'ring_direction_type.dart';
+
 /// Points to the top of the drawing circle from where to start painting the arc
 const _kStartAngleInRadians = 0 - math.pi / 2;
 
 class RingPainter extends CustomPainter {
-  /// Color to paint the ring with
-  final Color color;
-
   /// Radius of the circle
   final double radius;
-
-  /// Stroke with
-  final double strokeWidth;
 
   /// The sweep angle in radians
   final double sweepAngleInRadians;
 
+  /// Stroke with
+  final double strokeWidth;
+
+  /// Color to paint the ring with
+  final Color strokeColor;
+
+  /// An optional color for the background full circle
+  final Color? strokeFullCircleColor;
+
+  /// Direction to paint the ring
+  final RingDirectionType ringDirectionType;
+
   RingPainter({
-    required this.color,
     required this.radius,
-    required this.strokeWidth,
     required this.sweepAngleInRadians,
+    required this.strokeWidth,
+    required this.strokeColor,
+    this.strokeFullCircleColor,
+    this.ringDirectionType = RingDirectionType.clockwise,
   });
 
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint();
     paint.strokeWidth = strokeWidth;
-    paint.color = color;
+    paint.color = strokeColor;
     paint.style = PaintingStyle.stroke;
 
+    if (strokeFullCircleColor != null) {
+      paint.color = strokeFullCircleColor!;
+      canvas.drawCircle(Offset(size.width / 2, size.height / 2), radius - strokeWidth, paint);
+    }
+
+    paint.color = strokeColor;
     canvas.drawArc(
       Rect.fromLTRB(0 + strokeWidth, 0 + strokeWidth, size.width - strokeWidth, size.height - strokeWidth),
-      _kStartAngleInRadians,
+      (ringDirectionType == RingDirectionType.clockwise)
+          ? _kStartAngleInRadians
+          : _kStartAngleInRadians - sweepAngleInRadians,
       sweepAngleInRadians,
       false,
       paint,
     );
 
     paint.strokeWidth = strokeWidth / 2;
-    canvas.drawCircle(Offset(size.width / 2 + 3, 0 + strokeWidth), strokeWidth / 4, paint);
+    canvas.drawCircle(Offset(size.width / 2 + 2, 0 + strokeWidth), strokeWidth / 4, paint);
   }
 
   /// Repaint when arrow direction has changed
   @override
   bool shouldRepaint(RingPainter oldDelegate) =>
-      oldDelegate.color != color ||
+      oldDelegate.strokeColor != strokeColor ||
       oldDelegate.radius != radius ||
       oldDelegate.strokeWidth != strokeWidth ||
       oldDelegate.sweepAngleInRadians != sweepAngleInRadians;
---
+++ flutter_ui_components/flutter_ui_components/lib/src/components/score_ring/src/score_ring.dart
@@ -0,0 +1,91 @@
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'ring_direction_type.dart';
+import 'ring_painter.dart';
+
+const _kDefaultRingColor = Color(0xFF008800);
+const _kDefaultStrokeWidth = 10.0;
+
+/// Shows
+class ScoreRing extends StatelessWidget {
+  /// The max possible value of the scale,
+  /// so that we define a scale from 0 to [maxScore].
+  final double maxScore;
+
+  /// Score 0 to max
+  final double score;
+
+  /// Size (X and Y) of the widget to paint.
+  /// It has a square shape.
+  final double size;
+
+  /// Size (X and Y) of the svg image to draw im the middle of the graphic.
+  final double imageSize;
+
+  /// Stroke with
+  final double strokeWidth;
+
+  /// Color to paint the ring with
+  final Color strokeColor;
+
+  /// An optional color for the background full circle
+  final Color? strokeFullCircleColor;
+
+  /// Direction to paint the ring
+  final RingDirectionType ringDirectionType;
+
+  /// Optional SVG asset name for picture to put in the center of the graphic
+  final String iconSgvAssetName;
+
+  const ScoreRing({
+    required this.maxScore,
+    required this.score,
+    required this.size,
+    required this.imageSize,
+    this.strokeWidth = _kDefaultStrokeWidth,
+    this.strokeColor = _kDefaultRingColor,
+    this.strokeFullCircleColor,
+    this.ringDirectionType = RingDirectionType.clockwise,
+    String? iconSgvAssetName,
+    super.key,
+  }) : iconSgvAssetName = iconSgvAssetName ?? '';
+
+  // late final _loadSvgImageFuture = _loadSvgImage();
+  @override
+  Widget build(BuildContext context) {
+    if (score <= 0 || maxScore <= 0) {
+      return const SizedBox.shrink();
+    }
+
+    return CustomPaint(
+      key: const Key('score_ring'),
+      painter: RingPainter(
+        radius: size / 2,
+        sweepAngleInRadians: score / maxScore * (2 * math.pi),
+        strokeWidth: strokeWidth,
+        strokeColor: strokeColor,
+        strokeFullCircleColor: strokeFullCircleColor,
+        ringDirectionType: ringDirectionType,
+      ),
+      child: Container(
+        decoration: BoxDecoration(border: Border.all(color: Colors.transparent)),
+        child: SizedBox(
+          height: size,
+          width: size,
+          child: (iconSgvAssetName.isNotEmpty)
+              ? Center(
+                  child: SvgPicture.asset(
+                    iconSgvAssetName,
+                    height: imageSize,
+                    width: imageSize,
+                    package: 'flutter_ui_components',
+                  ),
+                )
+              : const SizedBox.shrink(),
+        ),
+      ),
+    );
+  }
+}
